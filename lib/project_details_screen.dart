3637d2db297c49ffa313e4c9e42d9aa17c61a57f
mariatesfaye <mariatesfaye67@gmail.com>
2024-02-16 22:53:46 +0300

    Update project_details_screen.dart

--- lib/project_details_screen.dart
+++ lib/project_details_screen.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ProjectDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // Extract project details from route arguments
     final Map<String, dynamic> arguments =
         ModalRoute.of(context).settings.arguments;
     final String projectName = arguments['projectName'];
     final String projectDescription = arguments['projectDescription'];
+    final String projectLead = arguments['projectLead'];
+    final String startDate = arguments['startDate'];
+    final String endDate = arguments['endDate'];
+    final List<String> technologies = arguments['technologies'];
+    final String projectLink = arguments['projectLink'];
 
     return Scaffold(
       appBar: AppBar(
@@ -27,7 +32,28 @@ class ProjectDetailsScreen extends StatelessWidget {
               projectDescription,
               style: TextStyle(fontSize: 16),
             ),
-            // Add more project details here
+            SizedBox(height: 10),
+            ListTile(
+              title: Text('Project Lead'),
+              subtitle: Text(projectLead),
+            ),
+            ListTile(
+              title: Text('Start Date'),
+              subtitle: Text(startDate),
+            ),
+            ListTile(
+              title: Text('End Date'),
+              subtitle: Text(endDate),
+            ),
+            Wrap(
+              children: technologies.map((tech) => Text(tech + ' ')).toList(),
+            ),
+            SizedBox(height: 10),
+            if (projectLink != null)
+              ElevatedButton(
+                onPressed: () => launch(projectLink),
+                child: Text('View Project Website'),
+              ),
           ],
         ),
       ),
